b3a9d2352298442166b9e70de477b23d5cd48cd7
kb18k <jackexi@gmail.com>
2021-09-10 20:32:13 +0800

    Initial Commit

---
+++ classic03/classic03App.swift
@@ -0,0 +1,17 @@
+//
+//  classic03App.swift
+//  classic03
+//
+//  Created by kai wen chen on 2021/9/10.
+//
+
+import SwiftUI
+
+@main
+struct classic03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
